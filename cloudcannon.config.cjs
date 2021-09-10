370956cb1ba58c0606d59434c8c95661e1892b11
Jonatah Benatti <jonatah@cloudcannon.com>
2021-09-10 17:44:02 +1200

    New go on config.

--- cloudcannon.config.cjs
+++ cloudcannon.config.cjs
@@ -1,77 +1,52 @@
 module.exports = {
-    // Global CloudCannon configuration
     _comments: {
       title: 'The title of your page.'
     },
   
+    paths: {
+      collections: 'content',
+      data: 'data',
+      static: 'public',
+      uploads: 'public/uploads'
+    },
+
     'data-config': {
       company: {
-        // Reads the contents of this file
         path: 'data/company.json'
       },
       footer: {
-        // Reads the contents of this file
         path: 'data/footer.json'
       },
       seo: {
-        // Reads the contents of this file
         path: 'data/seo.json'
       },
       site: {
-        // Reads the contents of this file
         path: 'data/site.json'
       }
     },
   
     'collections-config': {
       clients: {
-        // Reads the contents of each file in this directory
         path: 'content/clients',
         "output": true,
-
-        // How to parse the files in this collection
-        parser: 'front-matter',
-        
-        // CloudCannon collection-level configuration
         name: 'Clients',
-        _enabled_editors: ['data','content']
       },
-      pages: {
-        // Reads the contents of each file in this directory
+      webpages: {
         path: 'content/pages',
-
-        // How to parse the files in this collection
-        parser: 'front-matter',
-        
-        // CloudCannon collection-level configuration
+        url: '/[slug]',
+        "output": true,
         name: 'Pages'
       },
       'staff-members': {
-        // Reads the contents of each file in this directory
         path: 'content/staff-members',
         "output": true,
-
-        // How to parse the files in this collection
-        parser: 'front-matter',
-
-        // CloudCannon collection-level configuration
         name: 'Staff Members',
-        _enabled_editors: ['data','content']
       },
       posts: {
-        // Reads the contents of each file in this directory
         path: 'content/posts',
         "output": true,
-  
-        // How to parse the files in this collection
-        parser: 'front-matter',
-  
-        // The URL function for items in this collection
-        url: (filePath, frontMatter, filters) => {
-          const year = new Date(frontMatter.date).getFullYear();
-          const slug = filters.slugify(frontMatter.title || '');
-          return `/posts/${year}/${slug}/`;
-        }
+        url: '/blog/[slug]',
+        name: 'Blog'
       }
     }
   };
