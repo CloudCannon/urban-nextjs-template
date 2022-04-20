21b727427887d376eac5e83d91d9679ac7cca9c5
Jonatah Benatti <jonatah@cloudcannon.com>
2022-04-20 16:26:22 +1200

    Move to new _inputs configuration

--- cloudcannon.config.cjs
+++ cloudcannon.config.cjs
@@ -1,42 +1,44 @@
 module.exports = {
-    _comments: {
-      title: 'The title of your page.'
-    },
-    _select_data: {
-      categories: ['sales', 'tips', 'marketing', 'growth']
-    },
-    paths: {
-      collections: 'content',
-      data: 'data',
-      static: 'public',
-      uploads: 'public/uploads'
-    },
-    'collections-config': {
-      data: { path: 'data' },
-      clients: {
-        path: 'content/clients',
-        url: '/clients/[slug]',
-        "output": true,
-        name: 'Clients'
-      },
-      webpages: {
-        path: 'content/pages',
-        url: '/[slug]',
-        "output": true,
-        name: 'Pages',
-        _icon: 'wysiwyg',
-        _enabled_editors: ['visual','content']
-      },
-      'staff-members': {
-        path: 'content/staff-members',
-        _enabled_editors: ['data'],
-        name: 'Staff Members'
-      },
-      posts: {
-        path: 'content/posts',
-        "output": true,
-        url: '/blog/[slug]',
-        name: 'Blog'
-      }
+  _inputs: {
+    title: {
+      comment: 'The title of your page.'
     }
-  };
+  },
+  _select_data: {
+    categories: ['sales', 'tips', 'marketing', 'growth']
+  },
+  paths: {
+    collections: 'content',
+    data: 'data',
+    static: 'public',
+    uploads: 'public/uploads'
+  },
+  'collections-config': {
+    data: { path: 'data' },
+    clients: {
+      path: 'content/clients',
+      url: '/clients/[slug]',
+      "output": true,
+      name: 'Clients'
+    },
+    webpages: {
+      path: 'content/pages',
+      url: '/[slug]',
+      "output": true,
+      name: 'Pages',
+      _icon: 'wysiwyg',
+      _enabled_editors: ['visual', 'content']
+    },
+    'staff-members': {
+      path: 'content/staff-members',
+      _enabled_editors: ['data'],
+      name: 'Staff Members'
+    },
+    posts: {
+      path: 'content/posts',
+      "output": true,
+      url: '/blog/[slug]',
+      name: 'Blog'
+    }
+  }
+};
